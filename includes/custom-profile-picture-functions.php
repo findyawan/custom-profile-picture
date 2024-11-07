6ad66c8240134e687bda771de86953dc68001650
Farras Indyawan <findyawan@gmail.com>
2024-11-07 13:39:30 +0700

    fix profile image

--- includes/custom-profile-picture-functions.php
+++ includes/custom-profile-picture-functions.php
@@ -2,6 +2,11 @@
 
 defined('ABSPATH') or die('Direct script access disallowed.');
 
+/**
+ * Menambahkan field custom di halaman profil user untuk upload foto profil.
+ *
+ * @param WP_User $user Data user yang sedang diedit.
+ */
 function cpp_add_profile_fields($user) {
     ?>
     <h3><?php _e("Custom Profile Picture", "custom-profile-picture"); ?></h3>
@@ -22,17 +27,66 @@ function cpp_add_profile_fields($user) {
 add_action('show_user_profile', 'cpp_add_profile_fields');
 add_action('edit_user_profile', 'cpp_add_profile_fields');
 
+/**
+ * Menyimpan URL gambar profil ke user meta saat form profil disimpan.
+ *
+ * @param int $user_id ID user yang sedang diedit.
+ */
 function cpp_save_profile_fields($user_id) {
     if (!current_user_can('edit_user', $user_id)) {
         return false;
     }
 
-    update_user_meta($user_id, 'cpp_picture', $_POST['cpp_picture']);
+    if (isset($_POST['cpp_picture'])) {
+        update_user_meta($user_id, 'cpp_picture', $_POST['cpp_picture']);
+    }
 }
 
 add_action('personal_options_update', 'cpp_save_profile_fields');
 add_action('edit_user_profile_update', 'cpp_save_profile_fields');
 
+/**
+ * Mengganti avatar user dengan gambar profil yang telah dipilih dari Media Library.
+ *
+ * @param string $avatar HTML untuk avatar default.
+ * @param mixed $id_or_email User ID, email address, atau object.
+ * @param int $size Ukuran avatar.
+ * @param string $default URL ke default avatar.
+ * @param string $alt Alternatif teks untuk avatar.
+ * @return string HTML avatar yang diperbarui (jika ada gambar custom).
+ */
+function cpp_custom_avatar($avatar, $id_or_email, $size, $default, $alt) {
+    $user = false;
+
+    if (is_numeric($id_or_email)) {
+        $user_id = (int) $id_or_email;
+        $user = get_user_by('id', $user_id);
+    } elseif (is_object($id_or_email)) {
+        if (!empty($id_or_email->user_id)) {
+            $user_id = (int) $id_or_email->user_id;
+            $user = get_user_by('id', $user_id);
+        }
+    } elseif (is_string($id_or_email)) {
+        $user = get_user_by('email', $id_or_email);
+    }
+
+    if ($user && is_object($user)) {
+        $custom_avatar_url = get_user_meta($user->ID, 'cpp_picture', true);
+
+        if ($custom_avatar_url) {
+            $avatar = sprintf("<img alt='%s' src='%s' class='avatar avatar-%d photo' height='%d' width='%d' />",
+                       esc_attr($alt), esc_url($custom_avatar_url), (int)$size, (int)$size, (int)$size);
+        }
+    }
+
+    return $avatar;
+}
+
+add_filter('get_avatar', 'cpp_custom_avatar', 10, 5);
+
+/**
+ * Script JavaScript untuk menggunakan media uploader WordPress.
+ */
 function cpp_media_selector_script() {
     ?>
     <script type='text/javascript'>
